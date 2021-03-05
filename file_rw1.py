8cc8af9adceace1d7251e012de495ba79552bdfc
Wachira Duangdee <wcrcorp@gmail.com>
2021-03-05 12:10:25 +0700

    Create file_rw1.py

---
+++ file_rw1.py
@@ -0,0 +1,18 @@
+file = open("testfile.txt","w") 
+ 
+file.write("Hello World\n") 
+file.write("This is our new text file\n") 
+file.write("and this is another line.\n") 
+file.write("Why? Because we can.\n") 
+ 
+file.close() 
+
+#-----------read line------------
+# file = open("testfile.txt", "r")
+# print file.readline():
+#-----------------------
+
+#-----------read5str------------
+# file = open("testfile.txt", "r")
+# print file.read(5)
+#-----------------------
